3b18bcca1909fc8418427d448cecacda8e05a4f4
Shoeb-0204 <malikshoaib0402@gmail.com>
2022-08-05 00:35:47 +0530

    feat : updated

--- scripts/script.js
+++ scripts/script.js
@@ -3,29 +3,32 @@ var btnCheck = document.querySelector('#btn-check')
 var outputDiv = document.querySelector('.output')
 
 
-btnCheck.addEventListener('click',function check(){
-    var malik = calculate(Number(angleSum[0].value),Number(angleSum[1].value),Number(angleSum[2].value))
-    if(malik === 180){
-        outputDiv.innerHTML = 'The input angles makes a Triangle'
-    }else if(malik !=180){
-        outputDiv.innerText = `The input angles don't makes a Triangle`
-    }
-    isTriangle()
-   
+btnCheck.addEventListener('click', function check() {
+  var angles = calculate(Number(angleSum[0].value), Number(angleSum[1].value), Number(angleSum[2].value))
+  if (angles === 180) {
+    outputDiv.innerHTML = 'The input angles makes a Triangle'
+  }
+  else if (angles != 180) {
+    outputDiv.innerText = `The input angles doesn't make a Triangle`
+  }
+  isTriangle()
+
 }
 )
 
-function calculate (a,b,c){
-    let sum= a+b+c
-    return sum
+function calculate(a, b, c) {
+  let sum = a + b + c
+  return sum
 }
-function isTriangle(){
-   let angle1 =  angleSum[1].value
-   let angle2 =  angleSum[1].value
-   let angle3 = angleSum[1].value
-  if(angle1<0 || angle2<0 || angle3<0){
-    outputDiv.innerText = `The value should not be negative`
-  }else if(!angle1 || !angle2 || !angle3){
+function isTriangle() {
+  let angle1 = angleSum[1].value
+  let angle2 = angleSum[1].value
+  let angle3 = angleSum[1].value
+  if (angle1 < 0 || angle2 < 0 || angle3 < 0) {
+    outputDiv.innerText = `The values should not be negative`
+  }
+  else if (!angle1 || !angle2 || !angle3) {
     outputDiv.innerText = `Please enter all the fields`
   }
+
 }
